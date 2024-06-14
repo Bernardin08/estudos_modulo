9a00e50ea48acfcc9e60d81be9a0f87f349818c3
Bernardin08 <bernardokrunitzki@gmail.com>
2024-06-14 17:19:32 -0300

    primeiro commit

---
+++ main_program.py
@@ -0,0 +1,15 @@
+import sys
+sys.path.append("/Users/182400219/")
+import hello
+#criei um link que associou o método world ao modulo hello que é um arquivo separado do my_program
+
+'''
+#acessando a variavel do arquivo my_program
+hello.shark
+
+polvo = hello.Octopus("marlon", "red")
+polvo.tell_me_about_the_octopus()
+'''
+
+
+
